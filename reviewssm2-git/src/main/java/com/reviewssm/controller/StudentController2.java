6fd56c8cc962cafa6e691db47b3e0eb48ca370e9
dongjiangtao <dongjiangtaowy@163.com>
2020-12-24 17:42:07 +0800

    update conflict

--- reviewssm2-git/src/main/java/com/reviewssm/controller/StudentController2.java
+++ reviewssm2-git/src/main/java/com/reviewssm/controller/StudentController2.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -45,41 +44,6 @@ public class StudentController2 implements Controller {
 	}
 	
 	public String m4() {
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
-
-import com.reviewssm.pojo.Student;
-import com.reviewssm.service.IStudentService;
-import com.reviewssm.service.StudentService;
-
-//@Controller
-public class StudentController2 implements Controller {
-	
-	private IStudentService studentService;
-	
-	public void setStudentService(IStudentService studentService) {
-		this.studentService = studentService;
-	}
-
-//	private StudentService studentService;
-//
-//	public void setStudentService(StudentService studentService) {
-//		this.studentService = studentService;
-//	}
-
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		List<Student> findAllStudents = studentService.findAllStudents();
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("msg", findAllStudents);
-		mav.setViewName("welcome");
-		return mav;
-	}
-	
-	public String gittest() {
 		return null;
 	}
 	
@@ -88,7 +52,6 @@ public class StudentController2 implements Controller {
 	}
 	
 	public String m2() {
->>>>>>> branch 'master' of https://github.com/jiangtaodong/reviewproject.git
 		return null;
 	}
 	
